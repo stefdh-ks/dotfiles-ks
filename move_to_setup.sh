59f6f95e8c459731b322cc3f2cb5b07e8a002e42
Stef Dhaeseleer <stef.dhaeseleer@keysight.com>
2024-10-03 11:12:45 +0200

    gitconfig updated
    + fixed wrong copy

--- move_to_setup.sh
+++ move_to_setup.sh
@@ -4,7 +4,7 @@ cp ~/dotfiles-ks/.vimrc* ~/
 cp ~/dotfiles-ks/.zshrc* ~/
 cp ~/dotfiles-ks/.zprofile ~/
 cp -r ~/dotfiles-ks/.oh-my-zsh* ~/
-cp ~/dotfiles-ks/.gitconfig ~/.gitconfig
+cp ~/dotfiles-ks/gitconfigglobal ~/.gitconfig
 cp ~/dotfiles-ks/.dpsh_paths ~/
 cp -r ~/dotfiles-ks/.fzf.zsh
 cp -r ~/dotfiles-ks/.config/ ~/
